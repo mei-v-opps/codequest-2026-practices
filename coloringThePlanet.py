73f9b16744c3c573b13f863de5fedb39278373c1
mei-v-opps <meihuaqu22@gmail.com>
2026-02-26 11:23:50 -0600

    fixed and lowk plagarized some other guys code for coloring the planet
    apparently you need to run the temperature through the relative val formula twice?? i dont understand but ill figure out why

--- coloringThePlanet.py
+++ coloringThePlanet.py
@@ -1,37 +1,29 @@
 import math
 import sys
 
-def lerp(a,b,t):
-    scalet = 0.0
-    newpoint = []
-    for p in range(1,3):
-        var = (1-t)*a[p] + t*b[p]
-        newpoint.append(var)
+def findRelVal(t, min, max):
+    return (t - min)/(max - min)
 
+def lerp(a, b, t):
+    return (1-t)*a + t*b
 
 cases  = int(input(""))
 for x in range(cases):
-    line1 = list(map(int, input().split(" ")))
-    cs = []
-    controlPts = []
-    temp = 0
-    for i in range(line1[0]):
-        thisLine = list(map(float, input().split(" ")))
-        controlPts.append(thisLine)
-        cs.append(thisLine[0])
+    N, L, H = map(int, input().split(" "))
+    controlPoints = []
+    for i in range(N):
+        position, R, G, B = map(float, input().split())
+        R,G,B = map(int, (R, G, B))
+        controlPoints.append([position, R, G, B])
     temp = int(input())
-    tempscale = round((temp - line1[1])/(line1[2] - line1[1]), 2)
-    cs.append(tempscale)
-    cs.sort()
-    indexes = [cs.index(tempscale) - 1, cs.index(tempscale)]
-    newpoint = []
-    for p in range(1,4):
-        var = (1-tempscale)*controlPts[indexes[0]][p] + tempscale*controlPts[indexes[1]][p]
-        var = math.floor(var)
-        newpoint.append(var)
-    # strList = " ".join(newpoint)
-    for r in range(len(newpoint)):
-        if(r != len(newpoint)):
-            print(newpoint[r], end= " ")
-        else:
-            print(newpoint[r], "\n")
+    temp = findRelVal(temp, L, H)
+    for m in range(N-1):
+        if(temp >= controlPoints[m][0] and temp <= controlPoints[m+1][0]):
+            controlPoints = [controlPoints[m], controlPoints[m+1]]
+            break
+    relVal = findRelVal(temp, controlPoints[0][0], controlPoints[1][0])
+    newPoint = []
+    for n in range(1,4):
+        newPoint.append(math.floor(lerp(controlPoints[0][n], controlPoints[1][n], relVal)))
+    newPoint = list(map(str, newPoint))
+    print(" ".join(newPoint))
