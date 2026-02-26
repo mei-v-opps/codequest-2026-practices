f3a5a3c230c16864a1cf4cbf803bcf6f6f4e9656
mei-v-opps <meihuaqu22@gmail.com>
2026-02-25 19:29:38 -0600

    fixed trailing zeros

--- apollo13.py
+++ apollo13.py
@@ -17,14 +17,10 @@ for i in range(cases):
                 fangles[l] = round(360 + (fangles[l]-180.0), 2)
             else:
                 fangles[l] = round(fangles[l] - 180.0, 2)
-    for p in range(len(fangles)):
-        if(fangles[p] >= 100.00):
-            anglesString[p] = str(fangles[p])
-        elif(fangles[p] >= 10.00):
-            anglesString[p] = "".join(["0", str(fangles[p])])
-        else:
-            anglesString[p] = "".join(["00", str(fangles[p])])
     anglesString = list(map(str, fangles))
+    for o in range(len(fangles)):
+        fangles[o] = f"{fangles[o]:06.2f}"
     finalAngles.append(anglesString)
-for m in range(len(finalAngles)):
-    print(" ".join(finalAngles[m]))
+    print(" ".join(fangles))
+# for m in range(len(finalAngles)):
+#     print(" ".join(finalAngles[m]))
