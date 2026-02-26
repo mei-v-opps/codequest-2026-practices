4d64a7ac45be787f9ee24f23c670fa9e2981101c
mei-v-opps <meihuaqu22@gmail.com>
2026-02-25 19:07:56 -0600

    made the leading zeroes print

--- apollo13.py
+++ apollo13.py
@@ -6,10 +6,24 @@ for i in range(cases):
     anglesString = input("").split(" ")
     fangles = list(map(float, anglesString))
     for l in range(len(fangles)):
-        if(fangles[l] - 180 < 0):
-            fangles[l] = round(360 + (fangles[l]-180.0), 2)
+        if(fangles[l] < 0):
+            fangles[l] = round(360 + fangles[l], 2)
+            if(fangles[l] - 180 < 0):
+                fangles[l] = round(360 + (fangles[l]-180.0), 2)
+            else:
+                fangles[l] = round(fangles[l] - 180.0, 2)
         else:
-            fangles[l] = round(fangles[l] - 180.0, 2)
+            if(fangles[l] - 180 < 0):
+                fangles[l] = round(360 + (fangles[l]-180.0), 2)
+            else:
+                fangles[l] = round(fangles[l] - 180.0, 2)
+    for p in range(len(fangles)):
+        if(fangles[p] >= 100.00):
+            anglesString[p] = str(fangles[p])
+        elif(fangles[p] >= 10.00):
+            anglesString[p] = "".join(["0", str(fangles[p])])
+        else:
+            anglesString[p] = "".join(["00", str(fangles[p])])
     anglesString = list(map(str, fangles))
     finalAngles.append(anglesString)
 for m in range(len(finalAngles)):
